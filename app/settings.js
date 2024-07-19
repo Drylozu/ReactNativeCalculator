cf7119a9ca3610f5daf248df1d4fbeb3872f36bf
Drylozu <35281350+Drylozu@users.noreply.github.com>
2024-07-18 23:57:31 -0500

    add settings

--- app/settings.js
+++ app/settings.js
@@ -3,6 +3,15 @@ import { Ionicons } from '@expo/vector-icons';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
 
+const tileStyle = {
+  padding: 16,
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  borderColor: 'rgba(255, 255, 255, 0.1)',
+  borderBottomWidth: 1,
+};
+
 export default function Settings() {
   const { top, bottom } = useSafeAreaInsets();
 
@@ -17,7 +26,7 @@ export default function Settings() {
       <View
         style={{
           flexDirection: 'row',
-          alignContent: 'center',
+          alignItems: 'center',
           paddingTop: 4,
           paddingBottom: 14,
           paddingHorizontal: 20,
@@ -29,69 +38,58 @@ export default function Settings() {
         <TouchableOpacity onPress={() => router.dismiss()}>
           <Ionicons name="arrow-back" size={24} color="white" />
         </TouchableOpacity>
-        <Text style={{ color: 'white', fontSize: 18 }}>Settings</Text>
+        <Text style={{ color: 'white', fontSize: 22, fontWeight: 'bold' }}>
+          Settings
+        </Text>
       </View>
 
       <ScrollView
         style={{ flex: 1 }}
         contentContainerStyle={{ paddingBottom: bottom }}
       >
-        <View
-          style={{
-            width: '100%',
-            height: 150,
-            backgroundColor: 'green',
-          }}
-        />
-        <View
-          style={{
-            width: '100%',
-            height: 150,
-            backgroundColor: 'blue',
-          }}
-        />
-        <View
-          style={{
-            width: '100%',
-            height: 150,
-            backgroundColor: 'yellow',
-          }}
-        />
-        <View
-          style={{
-            width: '100%',
-            height: 150,
-            backgroundColor: 'purple',
-          }}
-        />
-        <View
-          style={{
-            width: '100%',
-            height: 150,
-            backgroundColor: 'green',
-          }}
-        />
-        <View
-          style={{
-            width: '100%',
-            height: 150,
-            backgroundColor: 'blue',
-          }}
-        />
-        <View
-          style={{
-            width: '100%',
-            height: 150,
-            backgroundColor: 'yellow',
-          }}
-        />
-        <View
-          style={{
-            width: '100%',
-            height: 150,
-            backgroundColor: 'purple',
-          }}
-        />
+        <View style={tileStyle}>
+          <View style={{ gap: 4 }}>
+            <Text style={{ color: 'white', fontSize: 18 }}>Selected Theme</Text>
+            <Text style={{ color: '#ccc', fontSize: 14 }}>
+              Theme by Drylozu
+            </Text>
+          </View>
+
+          <Text style={{ color: 'white', fontSize: 16 }}>
+            Dark (pre-installed)
+          </Text>
+        </View>
+
+        <View style={tileStyle}>
+          <View style={{ gap: 4 }}>
+            <Text style={{ color: 'white', fontSize: 18 }}>
+              Load a new theme
+            </Text>
+            <Text style={{ color: '#ccc', fontSize: 14 }}>
+              Open the file picker and select a YAML (.yaml, .yml) file
+            </Text>
+          </View>
+        </View>
+
+        <View style={tileStyle}>
+          <View style={{ gap: 4 }}>
+            <Text style={{ color: 'white', fontSize: 18 }}>Remove theme</Text>
+            <Text style={{ color: '#ccc', fontSize: 14 }}>
+              Removes the current theme
+            </Text>
+          </View>
+        </View>
+
+        <View style={tileStyle}>
+          <View style={{ gap: 4 }}>
+            <Text style={{ color: 'white', fontSize: 18 }}>
+              Made by Drylozu
+            </Text>
+            <Text style={{ color: '#ccc', fontSize: 14 }}>
+              Deivid's Calculator - v1.0.0
+            </Text>
+          </View>
+        </View>
       </ScrollView>
     </View>
   );
